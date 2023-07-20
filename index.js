87bb2d81d759b72f59f4944fadc7690e6ed658fc
Maycon Silva <mayconslv.contato@gmail.com>
2023-07-20 09:42:31 -0300

    Renderizando dados da API

--- index.js
+++ index.js
@@ -20,4 +20,27 @@ dataInput.addEventListener('keypress', (e) => {
   }
 })
 
+const apiKey = 'rkaCLJfqofxBmUUvwC0q4Fa8fVrhXQ111WQqvONI'
+const apodUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`
 
+
+function renderImage(imageProps) {
+  const title = document.getElementById('image-title')
+  const image = document.getElementById('image')
+  const description = document.getElementById('image-explanation')
+  
+  title.innerHTML = imageProps.title
+  image.src = imageProps.url
+  description.innerHTML = imageProps.explanation
+
+}
+
+async function getApodData () {
+  const response = await fetch(apodUrl)
+  const data = await response.json()
+  console.log(data)
+
+  renderImage(data)
+}
+
+getApodData()
